ea4bb95ad171440e05be3b87bfb0c0fb0cf18cbb
ThomasMontaño <viraclipscolo@gmail.com>
2026-08-12 16:31:50 -0500

    Sprint 1

---
+++ app.js
@@ -0,0 +1,24 @@
+function validFormFieldInput() {
+
+  const tareaInput = document.querySelector('#nombreTarea');
+  const descripcionInput = document.querySelector('#tareaDescripcion');
+  const fechaInput = document.querySelector('#date');
+  const categoriaInput = document.querySelector('#tareaCategoria');
+  const alertError = document.querySelector('#alertError');
+
+  //limpiar espacios en blanco con .trim()
+  const titulo = tareaInput.value.trim();
+  const descripcion = descripcionInput.value.trim();
+  const fecha = fechaInput.value.trim();
+  const categoria = categoriaInput.value.trim();
+
+
+  const task = {
+    titulo,
+    descripcion,
+    fecha,
+    categoria
+  };
+
+  return task;
+}
